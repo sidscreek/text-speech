753b37fa426b86b8b975e899a1f5786a0760e54b
SiddhantJain <siddhantjain027@gmail.com>
2024-07-25 15:03:36 +0530

    pause/resume funcitonality added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let isSpeaking = true;
 const textarea = document.querySelector("textarea");
 const submit = document.querySelector(".submit-btn");
 const clear = document.querySelector(".clear-btn");
@@ -5,9 +6,27 @@ const clear = document.querySelector(".clear-btn");
 const textSpeech = () =>{
     const synth = window.speechSynthesis;
     const text = textarea.value; 
+    if(!synth.speaking && text)
+    {
+        const utterance = new SpeechSynthesisUtterance(text);
+        synth.speak(utterance);
+    }
 
-    const utterance = new SpeechSynthesisUtterance(text);
-    synth.speak(utterance);
+    if(text.length > 5)
+    {
+        if(synth.speaking && isSpeaking)
+        {
+            //change the button text 
+            submit.innerText = "Pause";
+            synth.resume(); 
+            isSpeaking = false;
+        }
+        else{
+            submit.innerText = "Resume";
+            synth.pause();
+            isSpeaking = true;
+        }
+    }
 };
 
 const clearText = () =>{
@@ -16,3 +35,4 @@ const clearText = () =>{
 };
 submit.addEventListener("click", textSpeech);
 clear.addEventListener("click", clearText);
+
